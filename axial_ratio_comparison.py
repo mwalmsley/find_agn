d583ae7d6d39e5316def07e9ea21ca273dbeee13
Mike Walmsley <walmsleymk1@gmail.com>
2018-01-29 15:32:54 +0000

    Completed exploration notebook - looks like agn/liner do have tighter detection cone than star-forming galaxies, within the not-edge-on sample.

--- axial_ratio_comparison.py
+++ axial_ratio_comparison.py
@@ -1,7 +1,23 @@
 
 from astropy.table import Table
+from astropy.io import fits
 import pandas as pd
 import numpy as np
+import datetime
+
+
+def cache_nsa(nsa_loc, cache_loc, useful_cols):
+    print('Begin caching at {}'.format(current_time()))
+    nsa_loc = '/data/galaxy_zoo/decals/catalogs/nsa_v1_0_1.fits'
+    nsa = Table(fits.getdata(nsa_loc))
+    print('Table loaded at {}'.format(current_time()))
+    nsa = nsa[useful_cols].to_pandas()
+    new_names = [col.lower() for col in useful_cols]
+    renaming_dict = dict(zip(useful_cols, new_names))
+    nsa = nsa.rename(columns=renaming_dict)
+    nsa.to_csv(cache_loc, index=False)
+    print('Saved to csv at {}'.format(current_time()))
+    return nsa
 
 
 def get_agn_catalog(agn_catalog_loc):
@@ -9,17 +25,22 @@ def get_agn_catalog(agn_catalog_loc):
     # nothing else to do for now
     return catalog
 
-def get_nsa(nsa_loc, useful_cols, cache_loc=None):
-    # nsa = Table.read(nsa_loc, format='fits')
-    # nsa = nsa[useful_cols].to_pandas()
-    # nsa = pd.read_csv(nsa_loc, usecols=useful_cols, encoding='latin1')
-    nsa = pd.read_csv(nsa_loc, nrows=10000, encoding='latin1')
-    # if cache_loc is not None:
-    #     nsa.to_csv(cache_loc)
-    # nsa = nsa.rename([{col: col.lower()} for col in nsa.columns.values])
-    return nsa
 
-# nsa = get_nsa(nsa_loc)
-# print(catalog.head())
-# axial_hist = hv.Histogram(np.histogram(catalog['ra'], bins=24))
+def current_time():
+    return datetime.datetime.now().time()
 
+if __name__ == '__main__':
+    useful_nsa_columns = [
+        'IAUNAME',
+        'RA',
+        'DEC',
+        # 'BASTOKES',
+        # 'PHISTOKES',
+        'PETRO_BA50',
+        'PETRO_PHI50',
+        'PETRO_BA90',
+        'PETRO_PHI90'
+    ]
+    nsa_loc = '/data/galaxy_zoo/decals/catalogs/nsa_v1_0_1.fits'
+    nsa_cache_loc = '/data/galaxy_zoo/decals/catalogs/nsa_v1`_0_1_cached.csv'
+    nsa = cache_nsa(nsa_loc, nsa_cache_loc, useful_nsa_columns)
