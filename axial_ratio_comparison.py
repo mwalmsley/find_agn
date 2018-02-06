22222dd93fe888d3636e87a17b6f7c68939d3c90
Mike Walmsley <walmsleymk1@gmail.com>
2018-02-06 16:50:52 +0000

    Add agn matching MVP. I quickly ran into trouble: the SDSS catalog used by Heckman doesn't agree with NSA V1_0_1. The basic catalogs used by Galaxy Zoo and Heckman disagree about where galaxies are, even at low redshift.

--- axial_ratio_comparison.py
+++ axial_ratio_comparison.py
@@ -10,11 +10,21 @@ def cache_nsa(nsa_loc, cache_loc, useful_cols):
     print('Begin caching at {}'.format(current_time()))
     nsa_loc = '/data/galaxy_zoo/decals/catalogs/nsa_v1_0_1.fits'
     nsa = Table(fits.getdata(nsa_loc))
+    # split ELPETRO_FLUX
     print('Table loaded at {}'.format(current_time()))
+    sdss_bands = ['f', 'n', 'u', 'g', 'r', 'i', 'z']
+    elpetro_columns = ['elpetro_flux_{}'.format(band) for band in sdss_bands]
+    for n in range(len(elpetro_columns)):
+        col = elpetro_columns[n]
+        nsa[col] = [nsa[row]['ELPETRO_FLUX'][n] for row in range(len(nsa))]
+    useful_cols = useful_cols + elpetro_columns
+    print(useful_cols)
+    print(nsa[useful_cols][0])
     nsa = nsa[useful_cols].to_pandas()
     new_names = [col.lower() for col in useful_cols]
     renaming_dict = dict(zip(useful_cols, new_names))
     nsa = nsa.rename(columns=renaming_dict)
+    print('Saving Table at {}'.format(current_time()))
     nsa.to_csv(cache_loc, index=False)
     print('Saved to csv at {}'.format(current_time()))
     return nsa
